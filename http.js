3564e87626fd4ed2ffe41be0812b107da04938a5
Ubuntu <ubuntu@ip-172-31-22-68.ap-northeast-1.compute.internal>
2016-01-01 15:09:05 +0000

    HTTP server testing

--- http.js
+++ http.js
@@ -1,14 +1,21 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 
-var buf = fs.readFileSync('home.html');
 
-var str = buf.toString();
+//var buf = fs.readFileSync('home.html');
+
+//var str = buf.toString();
 
 
 http.createServer(function (req, res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end(str);
+    req.on('data', function(chunk){
+	console.log(chunk);
+
+    });
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
 }).listen(8000);
 
 console.log('Listening to port 8000.');

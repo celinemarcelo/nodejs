fc6dce82b87f38717c60a6b3f0463b8bddded0e9
Celine Marcelo <celinemarcelo@gmail.com>
2016-01-08 00:43:29 +0800

    Move dynamodb.js to dynamodb

---
+++ dynamodb/dynamodb.js
@@ -0,0 +1,33 @@
+var AWS = require('aws-sdk');
+
+AWS.config.update({
+  region: "ap-northeast-1",
+  endpoint: "http://localhost:8000"
+});
+
+
+var dynamodb = new AWS.DynamoDB();
+
+var params = {
+    TableName : "SensorRecords",
+    KeySchema: [       
+        { AttributeName: "sensorNumber", KeyType: "HASH"},  //Partition key
+        { AttributeName: "timestamp", KeyType: "RANGE" }  //Sort key
+    ],
+    AttributeDefinitions: [       
+        { AttributeName: "sensorNumber", AttributeType: "N" },
+        { AttributeName: "timestamp", AttributeType: "S" }
+    ],
+    ProvisionedThroughput: {       
+        ReadCapacityUnits: 10, 
+        WriteCapacityUnits: 10
+    }
+};	
+
+dynamodb.createTable(params, function(err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});

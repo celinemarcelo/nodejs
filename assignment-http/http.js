c867d2f29f0c76c14fa207630a7078c8d12a76c0
Ubuntu <ubuntu@ip-172-31-19-197.ap-northeast-1.compute.internal>
2016-01-10 16:49:41 +0000

    Finish Tito's assignment

--- assignment-http/http.js
+++ assignment-http/http.js
@@ -1,10 +1,7 @@
-var http = require('http');
-var url = require('url');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-
-
+var AWS = require('aws-sdk');
 
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
@@ -13,11 +10,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(8001);
 
-
-
 app.post('/', function(req, res){
-	console.log(req.body.sensorNumber);
-	console.log(req.body.temperature);
-
-
+    //console.log(req.body.sensorNumber);
+    //console.log(req.body.temperature);
+    require('./put')(Number(req.body.sensorNumber), Number(req.body.temperature));
 });

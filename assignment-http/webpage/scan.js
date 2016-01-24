c17367525caa5644dd2e54665051e583f954f110
Celine Marcelo <celinemarcelo@gmail.com>
2016-01-25 02:30:08 +0800

    Merged server.js into scan.js

--- assignment-http/webpage/scan.js
+++ assignment-http/webpage/scan.js
@@ -1,27 +1,37 @@
-var scan = function(){
-    var AWS = require('aws-sdk');
-    var conf = require('./config');
-    var fs = require('fs');
-    
-    AWS.config.update({region: conf.get('region'),
-		       endpoint: conf.get('endpoint')});
-    
-    var docClient = new AWS.DynamoDB.DocumentClient();
-    
-    var params = {
-	TableName: 'SensorRecords'
-    };
-    
-    docClient.scan(params, function(err, data) {
-	if (err) {
-            console.log(err); // an error occurred
-	} else {
-            //console.log("The Scan call evaluated " + data.ScannedCount + " items");
-            //console.log(data); // successful response
-	    
-	    fs.writeFile('data.json', JSON.stringify(data.Items, null, 4));
-	}
-    });
+var AWS = require('aws-sdk');
+var conf = require('./config');
+var fs = require('fs');
+var express = require('express');
+
+var app = express();
+
+var results;
+
+app.listen(8002);
+
+AWS.config.update({
+    region: conf.get('region'),
+    endpoint: conf.get('endpoint')
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+var params = {
+    TableName: 'SensorRecords'
 };
 
-module.exports = scan;
+
+app.get('/', function(req, res) {
+    docClient.scan(params, function(err, data) {
+        if (err) {
+            console.log(err); // an error occurred
+        } else {
+            //console.log("The Scan call evaluated " + data.ScannedCount + " items");
+            //console.log(data); // successful response
+            results = data.Items;
+            fs.writeFile('data.json', JSON.stringify(data.Items, null, 4));
+            console.log(results);
+            res.send(results);
+        }
+    });
+});
